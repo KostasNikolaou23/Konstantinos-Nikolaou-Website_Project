498cf735a720250d55423366d3b5db9c3e11fdce
Antonios <anthonyv156@outlook.com>
2025-04-09 20:02:49 +0300

    merged test css into main app css for simplicity

--- src/Pages/TestPage.jsx
+++ src/Pages/TestPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import './testpage.css';
+
 
 const shows = [
   {
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,31 @@
+import { useState } from "react";
+import '../App.css';
+
 export function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+
 	return (
-		<header>
-			<h1>My Website</h1>
-			<nav>
-				<ol>
-					<li><a href="/">Home</a></li>
-					<li><a href="/AlternativeMovies">Alternative Movies</a></li>
-					<li><a href="#contact">Contact</a></li>
-				</ol>
-			</nav>
-		</header>
+			<header className="header">
+        <h1>Movie Cave</h1>
+
+        <nav className="navbar">
+          <button className="menu-toggle" onClick={toggleMenu}>
+            ☰
+          </button>
+          <ul className={`menu ${menuOpen ? "open" : ""}`}>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Drama</a></li>
+            <li><a href="#">Documentaries</a></li>
+            <li><a href="#">Kids</a></li>
+            <li><a href="#">Live TV</a></li>
+          </ul>
+        </nav>
+        <div className="search-container">
+          <input type="text" placeholder="Search" className="search-input" />
+        </div>
+      </header>
 	);
 }
 
