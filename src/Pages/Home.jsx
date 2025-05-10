b41408f95bdc276c8dc15c78cf9afcac345e422c
Antonios <anthonyv156@outlook.com>
2025-05-10 16:54:57 +0300

    Fixed Home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,91 +1,97 @@
 import { useState, useEffect } from "react";
 import { getTrendingContent } from "../Components/API";
-import GridItem from "../Components/pages/Home/GridItem";
+import ContentCategorySelector from "../Components/pages/ContentCategorySelector";
 
 function Home() {
-    const [trendingContent, setTrendingContent] = useState({
-        movies: [],
-        tvseries: [],
-    });
+	return (
+		<div className="container fluid">
+			{/* Movies Section */}
+			<div className="row align-items-center">
+				<div className="col">
+					<h2>Movies</h2>
+				</div>
+				<div className="col"></div>
+				<div className="col d-flex justify-content-end">
+					<a href="/movies">
+						<button className="watch-button">View More</button>
+					</a>
+				</div>
+			</div>
+			<ContentCategorySelector
+				type="movies"
+				limit="4"
+				disableGenreSelection="true"
+			/>
 
-    useEffect(() => {
-        async function fetchContent() {
-            try {
-                // Fetch trending movies and TV series
-                const movies = await getTrendingContent("movies", 4);
-                const tvSeries = await getTrendingContent("tvseries", 4);
+			{/* TV Series Section */}
+			<div className="row align-items-center">
+				<div className="col">
+					<h2>TV Series</h2>
+				</div>
+				<div className="col"></div>
+				<div className="col d-flex justify-content-end">
+					<a href="/tvseries">
+						<button className="watch-button">View More</button>
+					</a>
+				</div>
+			</div>
+			<ContentCategorySelector
+				type="tvseries"
+				limit="4"
+				genreMap={{
+					10759: "Action & Adventure",
+					80: "Crime",
+					99: "Documentary",
+					18: "Drama",
+					9648: "Mystery",
+					10765: "Sci-Fi & Fantasy",
+				}}
+				disableGenreSelection="true"
+			/>
 
-                setTrendingContent({
-                    movies,
-                    tvseries: tvSeries,
-                });
-            } catch (error) {
-                console.error("Error fetching content:", error);
-            }
-        }
+			{/* Documentaries Section */}
+			<div className="row align-items-center">
+				<div className="col">
+					<h2>Documentaries</h2>
+				</div>
+				<div className="col"></div>
+				<div className="col d-flex justify-content-end">
+					<a href="/documentaries">
+						<button className="watch-button">View More</button>
+					</a>
+				</div>
+			</div>
+			<ContentCategorySelector
+				type="movies"
+				limit="4"
+				disableGenreSelection="true"
+				genreMap={{ 99: "Documentaries" }}
+			/>
 
-        fetchContent();
-    }, []);
-
-    return (
-        <div className="container fluid">
-            {/* Movies Section */}
-            <div className="row align-items-center">
-                <div className="col">
-                    <h2>Movies</h2>
-                </div>
-                <div className="col"></div>
-                <div className="col d-flex justify-content-end">
-                    <button className="watch-button">View More</button>
-                </div>
-            </div>
-            <section className="grid">
-                {trendingContent.movies.map((movie, index) => (
-                    <GridItem
-                        key={index}
-                        cardName={movie.title}
-                        cardDesc={movie.overview}
-                        cardImage={
-                            movie.poster_path
-                                ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
-                                : "path/to/fallback-image.jpg"
-                        }
-                        enableFavoriteButton={true} // Enable favorite button for movies
-                        enableReviewButton={true} // Enable review button for movies
-                    />
-                ))}
-            </section>
-
-            {/* TV Series Section */}
-            <div className="row align-items-center">
-                <div className="col">
-                    <h2>TV Series</h2>
-                </div>
-                <div className="col"></div>
-                <div className="col d-flex justify-content-end">
-                    <a href="/tvseries">
-                        <button className="watch-button">View More</button>
-                    </a>
-                </div>
-            </div>
-            <section className="grid">
-                {trendingContent.tvseries.map((series, index) => (
-                    <GridItem
-                        key={index}
-                        cardName={series.name} // Use `name` for TV series
-                        cardDesc={series.overview}
-                        cardImage={
-                            series.poster_path
-                                ? `https://image.tmdb.org/t/p/w500/${series.poster_path}`
-                                : "path/to/fallback-image.jpg"
-                        }
-                        enableFavoriteButton={true} // Enable favorite button for TV series
-                        enableReviewButton={true} // Enable review button for TV series
-                    />
-                ))}
-            </section>
-        </div>
-    );
+			{/* Kids Section */}
+			<div className="row align-items-center">
+				<div className="col">
+					<h2>Kids</h2>
+				</div>
+				<div className="col"></div>
+				<div className="col d-flex justify-content-end">
+					<a href="/kids">
+						<button className="watch-button">View More</button>
+					</a>
+				</div>
+			</div>
+			<ContentCategorySelector
+				type="tvseries"
+				limit="4"
+				disableGenreSelection="true"
+				genreMap={{
+					10751: "Family",
+					16: "Animation",
+					10762: "Kids",
+				}}
+			/>
+		</div>
+	);
 }
 
 export default Home;
