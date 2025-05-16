03574cfd18e87eb01398338c808138ea975d13e0
Konstantinos <dinosnikolaou24@gmail.com>
2025-05-16 18:02:14 +0300

    Updated Leaderboard
    Added medals to the first 3 users on the leaderboard

--- client/src/Pages/Leaderboard.jsx
+++ client/src/Pages/Leaderboard.jsx
@@ -29,6 +29,7 @@ export function Leaderboard() {
 
     return (
         <div className="container">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
             {error && <p style={{ color: "red" }}>Error: {error}</p>}
             <Table striped bordered hover size="sm" responsive="sm">
                 <thead>
@@ -46,27 +47,33 @@ export function Leaderboard() {
                 <tbody>
                     {leaderboard.map((user, index) => (
                         <tr key={user.userid}>
-                            {index === 0 ? (
-                                <>
-                                    <td className="table-active">{index + 1}</td>
-                                    <td className="table-active">
-                                        <center>{user.username}</center>
-                                    </td>
-                                    <td className="table-active">
-                                        <center>{user.achievement_count}</center>
-                                    </td>
-                                </>
-                            ) : (
-                                <>
-                                    <td>{index + 1}</td>
-                                    <td>
-                                        <center>{user.username}</center>
-                                    </td>
-                                    <td>
-                                        <center>{user.achievement_count}</center>
-                                    </td>
-                                </>
-                            )}
+                            <td className={index <= 2 ? "table-active" : ""}>{index + 1}</td>
+                            <td className={index <= 2 ? "table-active" : ""}>
+                                <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                                    {index === 0 && (
+                                        <i
+                                            className="fa-solid fa-medal"
+                                            style={{ color: "#FFD43B", marginRight: "8px", fontSize: "1.2em" }}
+                                        ></i>
+                                    )}
+                                    {index === 1 && (
+                                        <i
+                                            className="fa-solid fa-medal"
+                                            style={{ color: "#aaa9a7", marginRight: "8px", fontSize: "1.2em" }}
+                                        ></i>
+                                    )}
+                                    {index === 2 && (
+                                        <i
+                                            className="fa-solid fa-medal"
+                                            style={{ color: "#b36205", marginRight: "8px", fontSize: "1.2em" }}
+                                        ></i>
+                                    )}
+                                    <span>{user.username}</span>
+                                </div>
+                            </td>
+                            <td className={index <= 2 ? "table-active" : ""}>
+                                <div style={{ textAlign: "center" }}>{user.achievement_count}</div>
+                            </td>
                         </tr>
                     ))}
                 </tbody>
