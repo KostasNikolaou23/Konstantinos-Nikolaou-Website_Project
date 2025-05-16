f9b853d26a947d45fbe7ceb6ee5a39c283f6a665
Antonios <anthonyv156@outlook.com>
2025-05-16 17:29:34 +0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,66 +1,84 @@
-import { BrowserRouter as Router, Routes, Route, useParams } from 'react-router-dom';
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	useParams,
+} from "react-router-dom";
 
-import Header from './Components/Header';
-import Footer from './Components/Footer';
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 import "bootstrap/dist/css/bootstrap.min.css"; //Bootstrap CSS
 import "bootstrap/dist/js/bootstrap.bundle.min.js"; // Bootstrap JS
 
 // Pages
 // -----------------------------------------------------------
-import Home from './Pages/Home';
-import Leaderboard from './Pages/Leaderboard';
+import Home from "./Pages/Home";
+import Leaderboard from "./Pages/Leaderboard";
 
-import Movies from './Pages/Movies';
-import TVSeries from './Pages/TVSeries';
-import Kids from './Pages/Kids';
-import Documentaries from './Pages/Documentaries';
-import ContentPreview from './Pages/ContentPreview';
+import Movies from "./Pages/Movies";
+import TVSeries from "./Pages/TVSeries";
+import Kids from "./Pages/Kids";
+import Documentaries from "./Pages/Documentaries";
+import ContentPreview from "./Pages/ContentPreview";
 
 // Account Functionality
-import Login from './Pages/User/Login';
-import Register from './Pages/User/Register';
-import Track from './Pages/User/Track';
-import Achievements from './Pages/User/Achievements';
-import MyList from './Pages/User/MyList';
-import Profile from './Pages/User/Profile';
+import Login from "./Pages/User/Login";
+import Register from "./Pages/User/Register";
+import Track from "./Pages/User/Track";
+import Achievements from "./Pages/User/Achievements";
+import MyList from "./Pages/User/MyList";
+import Profile from "./Pages/User/Profile";
 
 function App() {
-  return (
-    <>
-      <Router>
-        <Header />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/settings" element={<div>Settings</div>} />
-            <Route path="/leaderboard" element={<Leaderboard />} />
+	return (
+		<>
+			<Router>
+				<Header />
+				<main>
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/settings" element={<div>Settings</div>} />
+						<Route path="/leaderboard" element={<Leaderboard />} />
 
-            {/* Account Functionality */}
-            <Route path="/user/login" element={<Login />} />
-            <Route path="/user/register" element={<Register />} />
-            <Route path="/user/track" element={<Track />} />
-            <Route path="/user/achievements" element={<Achievements />} />
-            <Route path="/user/mylist" element={<MyList />} />
-            <Route path="/user/profile" element={<Profile user_edit={true} />} />
-            <Route path="/user/logout" element={<div>Logout</div>} />
-            <Route path="/user/view/:username" element={<Profile user_edit={false} />} />
+						{/* Account Functionality */}
+						<Route path="/user/login" element={<Login />} />
+						<Route path="/user/register" element={<Register />} />
+						<Route path="/user/track" element={<Track />} />
+						<Route path="/user/achievements" element={<Achievements />} />
+						<Route path="/user/mylist" element={<MyList />} />
+						<Route path="/user/profile" element={<Profile user_edit={true} />} />
+						<Route path="/user/logout" element={<div>Logout</div>} />
+						<Route
+							path="/user/view/:username"
+							element={<Profile user_edit={false} />}
+						/>
 
-            {/* Content Pages */}
-            <Route path="/movies" element={<Movies />} />
-            <Route path="/tvseries" element={<TVSeries />} />
-            <Route path="/kids" element={<Kids />} />
-            <Route path="/documentaries" element={<Documentaries />} />
-            <Route path="/movie/:id" element={<ContentPreview type="movie" />} />
-            <Route path="/tvseries/:id" element={<ContentPreview type="tvseries" />} />
+						{/* Content Pages */}
+						<Route path="/movies" element={<Movies />} />
+						<Route path="/tvseries" element={<TVSeries />} />
+						<Route path="/kids" element={<Kids />} />
+						<Route path="/documentaries" element={<Documentaries />} />
+						<Route path="/movie/:id" element={<ContentPreview type="movie" />} />
+						<Route
+							path="/tvseries/:id"
+							element={<ContentPreview type="tvseries" />}
+						/>
 
-            <Route path="*" element={<div><center>404 Not Found</center></div>} />
-          </Routes>
-        </main>
-        <Footer />
-      </Router>
-    </>
-  );
+						<Route
+							path="*"
+							element={
+								<div>
+									<center>404 Not Found</center>
+								</div>
+							}
+						/>
+					</Routes>
+				</main>
+				<Footer />
+			</Router>
+		</>
+	);
 }
 
 export default App;
