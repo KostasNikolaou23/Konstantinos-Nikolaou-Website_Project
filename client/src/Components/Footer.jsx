84399b446866aa138891f43ff370d80927bc26bf
Konstantinos <dinosnikolaou24@gmail.com>
2025-05-17 19:30:30 +0300

    Update Footer.jsx

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -46,7 +46,7 @@ export function Footer() {
 
 								<FloatingLabel
 									controlId="floatingTextarea"
-									label="Enter your text here... Or not."
+									label="Enter your text here...."
 									className="mb-3"
 								>
 									<Form.Control as="textarea" placeholder="Leave a comment here" />
